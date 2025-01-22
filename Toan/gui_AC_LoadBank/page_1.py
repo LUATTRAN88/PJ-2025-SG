c749ac8102a3766b0bdbf207f56a4335f159d2c6
Toan-Course <toannguyenckd@gmail.com>
2025-01-22 17:58:36 +0700

    1/22/2025
    17h58

--- Toan/gui_AC_LoadBank/page_1.py
+++ Toan/gui_AC_LoadBank/page_1.py
@@ -11,27 +11,24 @@ class PAGE1:
     def __init__(self):
         self.origin_data = None
         self.is_on = False
+        self.is_test = False
         self.is_switch = False
         self.power_value = 0
         self.time_value = 0
     def create_layout(self,lay1):
-        
-        # self.layout_page = Frame(l1, bg='white')
-        # self.layout_page.place(x=0,y=64,width=1024,height=704)
-        
-        self.layout1 = Frame(lay1,bg='yellow')                     # 3 là 1
+        self.layout1 = Frame(lay1,bg='white')                   
         self.layout1.place(x=0,y=60,width=512,height=540)
 
-        self.layout2 = Frame(lay1,bg='pink')                        # 4 là 2
+        self.layout2 = Frame(lay1,bg='white')                        
         self.layout2.place(x=512,y=60,width=512,height=540)
         
-        self.lay_power_set = Frame(self.layout1,bg='red')
+        self.lay_power_set = Frame(self.layout1,bg='white')
         self.lay_power_set.place(x=0,y=0,width=512,height=180)
         
         self.lay_timer_set = Frame(self.layout1,bg='white')
         self.lay_timer_set.place(x=0,y=180,width=512,height=180)
         
-        self.lay_button_load = Frame(self.layout1,bg='grey')
+        self.lay_button_load = Frame(self.layout1,bg='white')
         self.lay_button_load.place(x=0,y=360,width=512,height=180)
         
         self.lay_power = Frame(self.layout2,bg='white')
@@ -40,18 +37,17 @@ class PAGE1:
         self.lay_parameter = Frame(self.layout2,bg='white')
         self.lay_parameter.place(x=0,y=180,width=512,height=180)
         
-        # self.lay_logo_switch = Frame(self.layout2,bg='white')
-        # self.lay_logo_switch.place(x=0,y=360,width=512,height=180)
-        
-        
-
-        # self.layout5 = Frame(self.layout1,bg='white')
-        # self.layout5.place(x=0,y=540,width=512,height=160)
+        self.lay_logo_switch = Frame(self.layout2,bg='white')
+        self.lay_logo_switch.place(x=0,y=360,width=512,height=180)
         
         self.label_powtime()
         self.tab_button()
         self.label_power()
         self.parameter()
+        self.button_fan()
+        self.button_test()
+        self.logo()
+        self.line()
         
         self.signal_object = []
         for i in range(16):
@@ -80,28 +76,34 @@ class PAGE1:
     def label_power(self):
         self.lb_running = Label(self.lay_power,bg='white',font=('arial bold',15),text='Running',fg='black').place(x=370,y=0,width=100,height=40)
        
-        self.lamp = Image.open(get_path_img()+'lamp.png')
+        self.lamp = Image.open(get_path_img()+'lamp_on.png').resize((17,17))
         self.piclamp = ImageTk.PhotoImage(self.lamp)
-        self.lb_lamp_run = Label(self.lay_power,bg='white',image=self.piclamp).place(x=460,y=2,width=40,height=40)
+        self.lb_lamp_run = Label(self.lay_power,bg='white',image=self.piclamp).place(x=460,y=0,width=40,height=40)
         
-        # self.tempcc = StringVar()
-        self.lb_temp = Label(self.lay_power,bg='red',font=('arial',13),text='35.2').place(x=423,y=36,width=46,height=22)  
-        self.lb_temp_c = Label(self.lay_power,bg='green',font=('arial',13),text='ºC').place(x=468,y=36,width=23,height=22) 
+        self.tempcc = StringVar()
+        self.lb_temp = Label(self.lay_power,bg='white',font=('arial',13),textvariable=self.tempcc).place(x=423,y=36,width=46,height=22)  
+        self.lb_temp_c = Label(self.lay_power,bg='white',font=('arial',13),text='ºC').place(x=468,y=36,width=23,height=22) 
         
-        self.lb_hour = Label(self.lay_power,bg='green',font=('arial',15),text='22').place(x=135,y=0,width=29,height=40)  
-        self.lb_2c = Label(self.lay_power,bg='red',font=('arial',15),text=':').place(x=161,y=0,width=5,height=40)  
-        self.lb_mins = Label(self.lay_power,bg='blue',font=('arial',15),text='44').place(x=166,y=0,width=29,height=40)  
+        self.lb_hour = Label(self.lay_power,bg='white',font=('arial',15),text='22').place(x=135,y=0,width=29,height=40)  
+        self.lb_2c = Label(self.lay_power,bg='white',font=('arial',15),text=':').place(x=161,y=0,width=5,height=40)  
+        self.lb_mins = Label(self.lay_power,bg='white',font=('arial',15),text='44').place(x=166,y=0,width=29,height=40)  
         
-        self.lb_day = Label(self.lay_power,bg='red',font=('arial',15),text='20').place(x=220,y=0,width=26,height=40)     
-        self.lb_x1 = Label(self.lay_power,bg='green',font=('arial',15),text='/').place(x=247,y=0,width=10,height=40)  
-        self.lb_month = Label(self.lay_power,bg='red',font=('arial',15),text='01').place(x=257,y=0,width=26,height=40) 
-        self.lb_x2 = Label(self.lay_power,bg='green',font=('arial',15),text='/').place(x=282,y=0,width=10,height=40) 
-        self.lb_year = Label(self.lay_power,bg='red',font=('arial',15),text='2025').place(x=292,y=0,width=50,height=40) 
+        self.lb_day = Label(self.lay_power,bg='white',font=('arial',15),text='20').place(x=220,y=0,width=26,height=40)     
+        self.lb_x1 = Label(self.lay_power,bg='white',font=('arial',15),text='/').place(x=247,y=0,width=10,height=40)  
+        self.lb_month = Label(self.lay_power,bg='white',font=('arial',15),text='01').place(x=257,y=0,width=26,height=40) 
+        self.lb_x2 = Label(self.lay_power,bg='white',font=('arial',15),text='/').place(x=282,y=0,width=10,height=40) 
+        self.lb_year = Label(self.lay_power,bg='white',font=('arial',15),text='2025').place(x=292,y=0,width=50,height=40) 
         
-        self.lb_power = Label(self.lay_power,bg='green',font=('arial bold',30),text='POWER',fg='red').place(x=25,y=75,width=150,height=30)
-        # self.tkw = StringVar()
-        self.lb_power_val = Label(self.lay_power,bg='green',font=('arial bold',49),text='58.1',fg='red').place(x=190,y=65,width=165,height=48)
-        self.lb_power_kw = Label(self.lay_power,bg='green',font=('arial bold',49),text='KW',fg='red').place(x=370,y=65,width=110,height=48)
+        self.lb_power = Label(self.lay_power,bg='white',font=('arial bold',30),text='POWER',fg='red').place(x=25,y=75,width=150,height=30)
+        self.tkw = StringVar()
+        self.lb_power_val = Label(self.lay_power,bg='white',font=('arial bold',49),textvariable=self.tkw,fg='red').place(x=190,y=65,width=165,height=48)
+        self.lb_power_kw = Label(self.lay_power,bg='white',font=('arial bold',49),text='KW',fg='red').place(x=370,y=65,width=110,height=48)
+        
+    def line(self):
+        self.lb_line_dt1 = Label(self.lay_power,bg='black').place(x=0,y=0,width=1,height=180)
+        self.lb_line_dt2 = Label(self.lay_logo_switch,bg='black').place(x=0,y=0,width=1,height=180)
+        self.lb_line_dt3 = Label(self.lay_timer_set,bg='black').place(x=0,y=0,width=512,height=1)
+        self.lb_line_dt4 = Label(self.lay_timer_set,bg='black').place(x=0,y=179,width=512,height=1)
         
     def parameter(self):
         # hàng line
@@ -176,22 +178,62 @@ class PAGE1:
         self.lb_fre_A = Label(self.lay_parameter,bg='white',font=('arial',15),text='232.4',fg='black').place(x=256,y=145,width=83,height=33)
         self.lb_fre_none5 = Label(self.lay_parameter,bg='white',font=('arial BOLD',15),text='A.PF',fg='black').place(x=341,y=145,width=83,height=33)
         self.lb_fre_ll_v = Label(self.lay_parameter,bg='white',font=('arial',15),text='0.99',fg='black').place(x=426,y=145,width=84,height=34)
+         
+    def button_fan(self):
+        a1 = Image.open(get_path_img()+'sw_on.png').resize((139,65))
+        self.on = ImageTk.PhotoImage(a1)
+        a2 = Image.open(get_path_img()+'sw_auto.png').resize((139,65))
+        self.off = ImageTk.PhotoImage(a2)
+
+        self.btn_on = Button(self.lay_logo_switch,image=self.on,bg='white', bd=0,command=lambda:self.button_fan())
+        self.btn_on.place(x=15,y=46,width=139,height=70)
+
+        if self.is_on:
+            self.btn_on.config(image=self.off)
+            self.is_on = False 
+        else :
+            self.btn_on.config(image=self.on)
+            self.is_on = True
+            
+    def button_test(self):
+        test1 = Image.open(get_path_img()+'sw_1p.png').resize((139,65))
+        self.ont = ImageTk.PhotoImage(test1)
+        test2 = Image.open(get_path_img()+'sw_3p.png').resize((139,65))
+        self.offt = ImageTk.PhotoImage(test2)
+
+        self.btn_on = Button(self.lay_logo_switch,image=self.ont,bg='white', bd=0,command=lambda:self.button_test())
+        self.btn_on.place(x=171,y=46,width=139,height=70)
+
+        if self.is_test:
+            self.btn_on.config(image=self.offt)
+            self.is_test = False 
+        else :
+            self.btn_on.config(image=self.ont)
+            self.is_test = True
+            
+    def logo(self):
+        logo = Image.open(get_path_img()+'logo.jpg').resize((117,117))
+        self.piclo = ImageTk.PhotoImage(logo)
+        self.label_logo = Label(self.lay_logo_switch, bg='red',image=self.piclo).place(x=350, y=22,width=117,height=117)
         
+        self.lb_fan_control = Label(self.lay_logo_switch, bg='white',fg='orange',font=('arial bold',10),text='FAN CONTROL').place(x=29,y=150,width=110,height=15)
+        self.lb_test_mode = Label(self.lay_logo_switch, bg='white',fg='orange',font=('arial bold',10),text='TEST MODE').place(x=185,y=150,width=110,height=15)
+        self.lb_logo_name = Label(self.lay_logo_switch, bg='white',fg='black',font=('arial bold',10),text='TLC ENGINEERING SOLUTIONS').place(x=308,y=150,width=198,height=15)
         
     def label_powtime(self):
-        self.lb_powset = Label(self.lay_power_set,bg='yellow',font=('arial bold',30),text='POWER\n SET',fg='#2E8B57').place(x=15,y=48,width=150,height=75)
-        self.lb_powset_kw = Label(self.lay_power_set,bg='white',font=('arial bold',45),text='KW',fg='#8B2252').place(x=295,y=69,width=100,height=45)
+        self.lb_powset = Label(self.lay_power_set,bg='white',font=('arial bold',30),text='POWER\n SET',fg='orange').place(x=15,y=48,width=150,height=75)
+        self.lb_powset_kw = Label(self.lay_power_set,bg='white',font=('arial bold',45),text='KW',fg='orange').place(x=295,y=69,width=100,height=45)
         
-        self.lb_powset_val = Label(self.lay_power_set,bg='pink',font=('arial bold',45),text='0',fg='#8B2252').place(x=175,y=69,width=105,height=45)
+        # self.lb_powset_val = Label(self.lay_power_set,bg='white',font=('arial bold',45),text='0',fg='orange').place(x=175,y=69,width=105,height=45)
 
-        self.lb_available = Label(self.lay_power_set,bg='purple',font=('arial bold',15),text='AVAILABLE POWER').place(x=15,y=145,width=197,height=20)
-        self.lb_available_val = Label(self.lay_power_set,bg='purple',font=('arial bold',15),text='99.8').place(x=235,y=145,width=65,height=20)
-        self.lb_available_val = Label(self.lay_power_set,bg='purple',font=('arial bold',15),text='KW').place(x=305,y=145,width=45,height=20)
+        self.lb_available = Label(self.lay_power_set,bg='white',font=('arial bold',15),text='AVAILABLE POWER').place(x=15,y=145,width=197,height=20)
+        self.lb_available_val = Label(self.lay_power_set,bg='white',font=('arial bold',15),text='99.8').place(x=235,y=145,width=65,height=20)
+        self.lb_available_val = Label(self.lay_power_set,bg='white',font=('arial bold',15),text='KW').place(x=305,y=145,width=45,height=20)
 
 
-        self.lb_timer = Label(self.lay_timer_set,bg='yellow',font=('arial bold',30),text='TIMER\n SET',fg='#2E8B57').place(x=15,y=48,width=120,height=76)
-        self.lb_timer_mins = Label(self.lay_timer_set,bg='red',font=('arial bold',45),text='mins',fg='#8B2252').place(x=265,y=69,width=135,height=45)
-        self.lb_timer_val = Label(self.lay_timer_set,bg='green',font=('arial bold',45),text='0',fg='#8B2252').place(x=150,y=69,width=100,height=45)
+        self.lb_timer = Label(self.lay_timer_set,bg='white',font=('arial bold',30),text='TIMER\n SET',fg='orange').place(x=15,y=48,width=120,height=76)
+        self.lb_timer_mins = Label(self.lay_timer_set,bg='white',font=('arial bold',45),text='mins',fg='orange').place(x=265,y=69,width=135,height=45)
+        # self.lb_timer_val = Label(self.lay_timer_set,bg='white',font=('arial bold',45),text='0',fg='orange').place(x=150,y=69,width=100,height=45)
 
       
     def tab_button(self):
@@ -200,38 +242,38 @@ class PAGE1:
         self.picup = ImageTk.PhotoImage(self.up)
         self.picdown = ImageTk.PhotoImage(self.down)
         
-        self.btn_powset_up = Button(self.lay_power_set,bd=3, bg='#20B2AA',image=self.picup,command=lambda:self.increase_power_set()).place(x=412,y=18,width=84,height=55)
-        self.btn_powset_down = Button(self.lay_power_set,bd=3,bg='#20B2AA',image=self.picdown,command=lambda:self.reduce_power_set()).place(x=412,y=108,width=84,height=55)
+        self.btn_powset_up = Button(self.lay_power_set,bd=3, bg='#191970',image=self.picup,command=lambda:self.increase_power_set()).place(x=412,y=18,width=84,height=55)
+        self.btn_powset_down = Button(self.lay_power_set,bd=3,bg='#191970',image=self.picdown,command=lambda:self.reduce_power_set()).place(x=412,y=108,width=84,height=55)
 
-        self.btn_time_up = Button(self.lay_timer_set,bd=3,bg='#20B2AA',image=self.picup,command=lambda:self.increase_timer_set()).place(x=412,y=18,width=84,height=55)
-        self.btn_time_down = Button(self.lay_timer_set,bd=3,bg='#20B2AA',image=self.picdown,command=lambda:self.reduce_timer_set()).place(x=412,y=108,width=84,height=55)
+        self.btn_time_up = Button(self.lay_timer_set,bd=3,bg='#191970',image=self.picup,command=lambda:self.increase_timer_set()).place(x=412,y=18,width=84,height=55)
+        self.btn_time_down = Button(self.lay_timer_set,bd=3,bg='#191970',image=self.picdown,command=lambda:self.reduce_timer_set()).place(x=412,y=108,width=84,height=55)
 
         
-        self.btn_apply = Button(self.lay_button_load,bd=3,bg='#20B2AA',font=('arial bold',12),text='LOAD APPLY',fg='#8B2252')
+        self.btn_apply = Button(self.lay_button_load,bd=3,bg='#191970',font=('arial bold',12),text='LOAD APPLY',fg='white')
         self.btn_apply.place(x=21,y=6,width=150,height=48)
-        self.btn_stop = Button(self.lay_button_load,bd=3,bg='#20B2AA',font=('arial bold',12),text='LOAD STOP',fg='#8B2252')
+        self.btn_stop = Button(self.lay_button_load,bd=3,bg='#191970',font=('arial bold',12),text='LOAD STOP',fg='white')
         self.btn_stop.place(x=182,y=6,width=150,height=48)
-        self.btn_drop = Button(self.lay_button_load,bd=3,bg='#20B2AA',font=('arial bold',12),text='LOAD DROP',fg='#8B2252')
+        self.btn_drop = Button(self.lay_button_load,bd=3,bg='#191970',font=('arial bold',12),text='LOAD DROP',fg='white')
         self.btn_drop.place(x=343,y=6,width=150,height=48)
-        self.btn_logging = Button(self.lay_button_load,bd=3,bg='#20B2AA',font=('arial bold',12),text='LOAD LOGGING',fg='#8B2252')
+        self.btn_logging = Button(self.lay_button_load,bd=3,bg='#191970',font=('arial bold',12),text='LOAD LOGGING',fg='white')
         self.btn_logging.place(x=21,y=66,width=150,height=48)
         
     def increase_power_set(self):
         if self.power_value < 150:
             self.power_value += 1
-            self.lb_powset_val = Label(self.lay_power_set,bg='pink',font=('arial bold',45),text=str(self.power_value),fg='#8B2252').place(x=175,y=69,width=105,height=45)
+            self.lb_powset_val = Label(self.lay_power_set,bg='white',font=('arial bold',45),text=str(self.power_value),fg='orange').place(x=175,y=69,width=105,height=45)
     def reduce_power_set(self):
         if self.power_value > 0:
             self.power_value -= 1
-            self.lb_powset_val = Label(self.lay_power_set,bg='pink',font=('arial bold',45),text=str(self.power_value),fg='#8B2252').place(x=175,y=69,width=105,height=45)        
+            self.lb_powset_val = Label(self.lay_power_set,bg='white',font=('arial bold',45),text=str(self.power_value),fg='orange').place(x=175,y=69,width=105,height=45)        
     def increase_timer_set(self):
         if self.time_value < 60:
             self.time_value +=1
-            self.lb_timer_val = Label(self.lay_timer_set,bg='green',font=('arial bold',45),text=str(self.time_value),fg='#8B2252').place(x=150,y=69,width=100,height=45)        
+            self.lb_timer_val = Label(self.lay_timer_set,bg='white',font=('arial bold',45),text=str(self.time_value),fg='orange').place(x=150,y=69,width=100,height=45)        
     def reduce_timer_set(self):
         if self.time_value > 0:
             self.time_value -=1
-            self.lb_timer_val = Label(self.lay_timer_set,bg='green',font=('arial bold',45),text=str(self.time_value),fg='#8B2252').place(x=150,y=69,width=100,height=45)    
+            self.lb_timer_val = Label(self.lay_timer_set,bg='white',font=('arial bold',45),text=str(self.time_value),fg='orange').place(x=150,y=69,width=100,height=45)    
         
 class SIGNAL:
     def __init__(self):
@@ -240,13 +282,13 @@ class SIGNAL:
         self.layout =  Frame(lay_button_load,bg='white')
         self.layout.place(x=x+2,y=124, width=28,height=46)
 
-        self.photol = Image.open(get_path_img()+'lamp.png').resize((28,28))
+        self.photol = Image.open(get_path_img()+'lamp_on.png').resize((17,17))
         self.picl = ImageTk.PhotoImage(self.photol)
         self.lb_lamp = Label(self.layout, bg='white',image=self.picl)
-        self.lb_lamp.place(x=0,y=12.5,width=28,height=23)
+        self.lb_lamp.place(x=0,y=12,width=28,height=23)
         
         
-        self.lb_relay = Label(self.layout, bg='red',font=('arial bold',8),fg='black',text=text).place(x=0,y=0,width=28,height=11)
+        self.lb_relay = Label(self.layout, bg='white',font=('arial bold',8),fg='black',text=text).place(x=0,y=0,width=28,height=11)
 
         self.text_relay_val = StringVar()
-        self.lb_relay_val = Label(self.layout, bg='red',font=('arial bold',8),fg='black',textvariable=self.text_relay_val).place(x=0,y=36,width=28,height=11)
+        self.lb_relay_val = Label(self.layout, bg='white',font=('arial bold',8),fg='black',textvariable=self.text_relay_val).place(x=0,y=36,width=28,height=11)
