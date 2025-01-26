af838f4124482ac44e7fbed28afa634f3c07f66f
Toan-Course <toannguyenckd@gmail.com>
2025-01-26 18:48:41 +0700

    1/26/2025
    18h48

--- Toan/gui_AC_LoadBank/main.py
+++ Toan/gui_AC_LoadBank/main.py
@@ -7,7 +7,7 @@ from page_5 import *
 
 class MAINGUI:
     def __init__(self):
-        pass
+        self.current_page = None
     def create_layout(self):
         self.layout = Tk()
         self.layout.title('AC LOAD BANK')
@@ -19,41 +19,59 @@ class MAINGUI:
         self.layout_btn.place(x=0,y=0,width=1024,height=60)
 
         self.button_main()
+        self.current_page = None
+        self.event_page1(None)
         
-        self.display1 = PAGE1()
-        self.display1.create_layout(self.layout)
+        # self.display1 = PAGE1()
+        # self.display1.create_layout(self.layout)
         
         self.btn_auto_test.bind('<Button-1>', self.event_page1)
         self.btn_manual_test.bind('<Button-1>', self.event_page2)
         self.btn_setting.bind('<Button-1>', self.event_page3)
         self.btn_service.bind('<Button-1>', self.event_page4)
         
-        
     def event_page1(self,event):
-        self.display1 = PAGE1()
-        self.display1.create_layout(self.layout) 
+        self._destroy_current_page()
+        display1 = PAGE1()
+        display1.create_layout(self.layout) 
+        self.current_page = display1
+        
     def event_page2(self,event):
+        self._destroy_current_page()
         display2 = PAGE2()
         display2.create_layout(self.layout)
+        self.current_page = display2
+        
     def event_page3(self,event):
+        self._destroy_current_page()
         display3 = PAGE3()
         display3.create_layout(self.layout)
+        self.current_page = display3
+        
     def event_page4(self,event):
+        self._destroy_current_page()
         display4 = PAGE4()
         display4.create_layout(self.layout)
-    
-
+        self.current_page = display4
+         
+    def _destroy_current_page(self):
+        if self.current_page is not None:
+            for widget in self.layout.winfo_children():
+                if widget != self.layout_btn:
+                    widget.destroy()     
+        
+        
     def button_main(self):
-        self.btn_auto_test = Button(self.layout_btn,bd=3,fg='#7D7D7D', bg='white',font=('Arial Bold',20),text='AUTO TEST',command=lambda:self.btn_light_auto())
+        self.btn_auto_test = Button(self.layout_btn,bd=3,fg='#7D7D7D', bg='white',font=('Arial Bold',20),text='AUTO TEST',command=self.btn_light_auto)
         self.btn_auto_test.place(x=0,y=0,width=256,height=60)
 
-        self.btn_manual_test = Button(self.layout_btn,bd=3,fg='#7D7D7D', bg='white',font=('Arial Bold',20),text='MANUAL TEST',command=lambda:self.btn_light_manual())
+        self.btn_manual_test = Button(self.layout_btn,bd=3,fg='#7D7D7D', bg='white',font=('Arial Bold',20),text='MANUAL TEST',command=self.btn_light_manual)
         self.btn_manual_test.place(x=256,y=0,width=256,height=60)
 
-        self.btn_setting = Button(self.layout_btn,bd=3,fg='#7D7D7D', bg='white',font=('Arial Bold',20),text='SETTING',command=lambda:self.btn_light_setting())
+        self.btn_setting = Button(self.layout_btn,bd=3,fg='#7D7D7D', bg='white',font=('Arial Bold',20),text='SETTING',command=self.btn_light_setting)
         self.btn_setting.place(x=512,y=0,width=256,height=60)
 
-        self.btn_service = Button(self.layout_btn,bd=3,fg='#7D7D7D',bg='white',font=('Arial Bold',20),text='SERVICE',command=lambda:self.btn_light_service())
+        self.btn_service = Button(self.layout_btn,bd=3,fg='#7D7D7D',bg='white',font=('Arial Bold',20),text='SERVICE',command=self.btn_light_service)
         self.btn_service.place(x=768,y=0,width=256,height=60)
 
     def btn_light_auto(self):
@@ -82,6 +100,46 @@ display = MAINGUI()
 display.create_layout()
 
 mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
